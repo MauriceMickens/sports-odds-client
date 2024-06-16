df33c397685a10dfda0e5de92d4694d011875f2b
Maurice Mickens <mauricetmickens@gmail.com>
2024-06-16 17:53:59 -0400

    Initial Commit

---
+++ sports-odds-client/sports_odds_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  sports_odds_clientApp.swift
+//  sports-odds-client
+//
+//  Created by Mickens on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct sports_odds_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

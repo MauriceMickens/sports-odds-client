c2d60df72416f4c1234fe519af6098b2985919ac
Maurice Mickens <mauricetmickens@gmail.com>
2024-07-04 11:51:02 -0400

    Add Firebase SDK

--- sports-odds-client/sports_odds_clientApp.swift
+++ sports-odds-client/sports_odds_clientApp.swift
@@ -5,10 +5,14 @@
 //  Created by Mickens on 6/16/24.
 //
 
+import Firebase
+import FirebaseCore
 import SwiftUI
 
 @main
 struct sports_odds_clientApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var appState = AppState()
     
     var body: some Scene {
@@ -19,3 +23,12 @@ struct sports_odds_clientApp: App {
     }
 }
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
